78674b8b4a686896e07b5475c75b5de895b594fd
Jacqueline Park <jacky.park@kellogg.northwestern.edu>
2023-05-29 21:19:54 +0000

    working on authentication in sessions_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     puts "------------------ code before every request ------------------"
+    @current_user = User.find_by({ "id" => session["email"] })
   end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["username"]}."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Unsuccessful login. Check your password again"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Unseccessful login. Check your email again"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
