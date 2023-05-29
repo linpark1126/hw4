00bb31ca37a538970e200db8e5d5c9cf9e35d64c
Jacqueline Park <jacky.park@kellogg.northwestern.edu>
2023-05-29 20:09:20 +0000

    edited routes / sessions new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get "/", :controller => "places", :action => "index"
+  get "/", :controller => "users", :action => "new"
   resources "places"
   resources "posts"
   resources "sessions"
   resources "users"
+
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+
 end
