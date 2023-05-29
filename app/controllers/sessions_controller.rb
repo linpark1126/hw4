1ca7771573794a3876791347dc8b250f897a6295
Jacqueline Park <jacky.park@kellogg.northwestern.edu>
2023-05-29 22:54:03 +0000

    Backend done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
         flash["notice"] = "Welcome, #{@user["username"]}"
-        redirect_to "/"
+        redirect_to "/places"
       else
         flash["notice"] = "Unsuccessful login. Check your password again"
         redirect_to "/sessions/new"
